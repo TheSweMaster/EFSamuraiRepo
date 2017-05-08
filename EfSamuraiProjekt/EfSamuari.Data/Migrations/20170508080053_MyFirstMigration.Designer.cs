ee61e19cc6642bf778ae2364a90e71ce36062464
TheSweMaster <theswemaster@gmail.com>
2017-05-08 10:09:03 +0200

    added first mimgration and update

---
+++ EfSamuraiProjekt/EfSamuari.Data/Migrations/20170508080053_MyFirstMigration.Designer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using EFSamurai.Data;
+
+namespace EfSamuari.Data.Migrations
+{
+    [DbContext(typeof(SamuraiContext))]
+    [Migration("20170508080053_MyFirstMigration")]
+    partial class MyFirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("EFSamurai.Domain.Samurai", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Samurais");
+                });
+        }
+    }
+}
