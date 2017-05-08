c708b03024f0479d7b7e3d9b0301a47959248623
TheSweMaster <theswemaster@gmail.com>
2017-05-08 09:55:16 +0200

    Init Samurai Repo

---
+++ EfSamuraiProjekt/EFSamurai.Domain/Samurai.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFSamurai.Domain
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+
+    }
+}
---
+++ EfSamuraiProjekt/EfSamuari.Data/SamuraiContext.cs
@@ -0,0 +1,20 @@
+﻿using EFSamurai.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFSamurai.Data
+{
+    public class SamuraiContext : DbContext 
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Server = (localdb)\\mssqllocaldb; Database = EfSamurai; Trusted_Connection = True; ");
+        }
+    }
+}
