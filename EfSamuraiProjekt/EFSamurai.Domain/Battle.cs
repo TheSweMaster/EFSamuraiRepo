b75e9118552ee50d37ef510c621516ad17191224
TheSweMaster <theswemaster@gmail.com>
2017-05-08 13:51:53 +0200

    Added battles

---
+++ EfSamuraiProjekt/EFSamurai.Domain/Battle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFSamurai.Domain
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Brutal { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
--- EfSamuraiProjekt/EfSamuari.Data/Migrations/20170508115113_MigrationBattles.Designer.cs
+++ EfSamuraiProjekt/EfSamuari.Data/Migrations/20170508115113_MigrationBattles.Designer.cs
@@ -9,9 +9,10 @@ using EFSamurai.Domain;
 namespace EfSamuari.Data.Migrations
 {
     [DbContext(typeof(SamuraiContext))]
-    partial class SamuraiContextModelSnapshot : ModelSnapshot
+    [Migration("20170508115113_MigrationBattles")]
+    partial class MigrationBattles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
